0bb8ffad41be943d883f2abf3fb2b9ab3b919d67
nitish4445 <nitishkumar09598@gmail.com>
2023-06-07 18:19:15 +0530

    done

--- App.js
+++ App.js
@@ -1,1 +1,136 @@
+const todos = document.querySelectorAll(".todo");
+const all_status = document.querySelectorAll(".status");
+let draggableTodo = null;
 
+todos.forEach((todo) => {
+    todo.addEventListener("dragstart", dragStart);
+    todo.addEventListener("dragend", dragEnd);
+});
+
+function dragStart() {
+    draggableTodo = this;
+    setTimeout(() => {
+        this.style.display = "none";
+    }, 0);
+    console.log("dragStart");
+}
+
+function dragEnd() {
+    draggableTodo = null;
+    setTimeout(() => {
+        this.style.display = "block";
+    }, 0);
+    console.log("dragEnd");
+}
+
+all_status.forEach((status) => {
+    status.addEventListener("dragover", dragOver);
+    status.addEventListener("dragenter", dragEnter);
+    status.addEventListener("dragleave", dragLeave);
+    status.addEventListener("drop", dragDrop);
+});
+
+function dragOver(e) {
+    e.preventDefault();
+    //   console.log("dragOver");
+}
+
+function dragEnter() {
+    this.style.border = "1px dashed #ccc";
+    console.log("dragEnter");
+}
+
+function dragLeave() {
+    this.style.border = "none";
+    console.log("dragLeave");
+}
+
+function dragDrop() {
+    this.style.border = "none";
+    this.appendChild(draggableTodo);
+    console.log("dropped");
+}
+
+/* modal */
+const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector(btn.dataset.targetModal).classList.add("active");
+        overlay.classList.add("active");
+    });
+});
+
+close_modals.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const modal = btn.closest(".modal");
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
+
+window.onclick = (event) => {
+    if (event.target == overlay) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => modal.classList.remove("active"));
+        overlay.classList.remove("active");
+    }
+};
+
+/* create todo  */
+const todo_submit = document.getElementById("todo_submit");
+
+todo_submit.addEventListener("click",createTodo );
+
+function createTodo() {
+    const todo_div = document.createElement("div");
+    let input_val = document.getElementsByClassName("todo_inputs");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("div");
+    console.log(input_val);
+    h3.textContent = input_val[0].value;
+    p.textContent = input_val[1].value;
+    h3.setAttribute('class', 'inline')
+    p.setAttribute('class', 'ellipsis')
+    // const txt = document.createTextNode(dis);
+
+
+    todo_div.appendChild(h3);
+    todo_div.appendChild(p);
+
+    todo_div.classList.add("todo");
+    todo_div.setAttribute("draggable", "true");
+
+    /* create span */
+    const span = document.createElement("span");
+    const span_txt = document.createTextNode("\u00D7");
+    span.classList.add("close");
+    span.appendChild(span_txt);
+
+    todo_div.appendChild(span);
+
+    no_status.appendChild(todo_div);
+
+    span.addEventListener("click", () => {
+        span.parentElement.style.display = "none";
+    });
+    //   console.log(todo_div);
+
+    todo_div.addEventListener("dragstart", dragStart);
+    todo_div.addEventListener("dragend", dragEnd);
+    let todo_form = document.getElementById('todo_form')
+    document.getElementsByClassName("todo_inputs")[0].value = "";
+    document.getElementsByClassName("todo_inputs")[1].value = "";
+    todo_form.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+const close_btns = document.querySelectorAll(".close");
+
+close_btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        btn.parentElement.style.display = "none";
+    });
+});
